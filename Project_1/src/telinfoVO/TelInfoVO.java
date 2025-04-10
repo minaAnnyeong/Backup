98a588b0bc720c657fff55b32cda2b54714ff2ba
minaAnnyeong <minaya257@gmail.com>
2025-04-10 15:05:43 +0900

    .

---
+++ Project_1/src/telinfoVO/TelInfoVO.java
@@ -0,0 +1,54 @@
+package telinfoVO;
+
+import java.util.Date;
+
+public class TelInfoVO {
+	private int id;
+	private String name;
+	private String tel;
+	private Date d;
+	
+	public TelInfoVO() { }
+	
+	public TelInfoVO(int id, String name, String tel, Date d){
+		super();
+		this.id = id;
+		this.name = name;
+		this.tel = tel;
+		this.d = d;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public Date getD() {
+		return d;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public void setD(Date d) {
+		this.d = d;
+	}
+	
+	
+}
