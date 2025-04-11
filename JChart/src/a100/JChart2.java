4015439cc4a01ab22d8e0596944b8fda0f11dba8
minaAnnyeong <minaya257@gmail.com>
2025-04-11 13:35:14 +0900

    .

---
+++ JChart/src/a100/JChart2.java
@@ -0,0 +1,48 @@
+package a100;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class JChart2 {
+
+	public static void main(String[] args) {
+		// category plot ==> bar chart
+		// XY plot ==> line chart
+		
+		// 차트에 들어갈 데이터를 dataset으로 저장
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+		dataset.addValue(100, "kor", "1mon");
+		dataset.addValue(50, "kor", "2mon");
+		dataset.addValue(80, "kor", "3mon");
+		dataset.addValue(70, "kor", "4mon");
+		dataset.addValue(60, "kor", "5mon");
+		
+		// LineChart pc2 = new LineChart(); // render 없이
+		// rendering : 프로그램으로 이미지 생성 프로세스
+		PlotOrientation po1 = PlotOrientation.VERTICAL; // 수직 그래프
+		JFreeChart jfchart = 
+				ChartFactory.createLineChart3D
+				("국어점수 월별 결과", "wol", "jum", dataset, po1, true, true, true);
+				//	title		기준 축   값 축 
+		// ChartFactory.createLineChart3D(parameters ... )
+		// ChartFactory : 다양한 유형의 차트 생성, dataset 사용
+		// ChartUtilities 클래스 : 차트를 이미지로 변환하는 
+		
+		// 한글 깨짐 방지
+		jfchart.getTitle().setFont(new Font("굴림", Font.BOLD, 15));
+		
+		jfchart.setBackgroundPaint(Color.yellow); 
+		jfchart.setBorderVisible(true);	// 경계선 보이게
+		jfchart.setBorderPaint(Color.PINK); // 경계선 색 지정
+		jfchart.setBorderStroke(new BasicStroke(15)); // 경계선 두께 지정
+		
+	}
+
+}
